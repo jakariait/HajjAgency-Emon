c8b9185e7575f106717a8e1020dd871669590836
Jakaria <jakariait@gmail.com>
2026-02-03 19:15:08 +0600

    Update

--- frontend/src/utils/brand.js
+++ frontend/src/utils/brand.js
@@ -1,7 +1,7 @@
-export const getBrandName = () => "TESS CORE LLC";
-export const getHomePageTitle = () => "TESS CORE LLC";
-export const getHomePageDescription = () => "TESS CORE LLC";
-export const getBrandLogo = () => "/WhatsApp_Image_2026-01-21_at_11.59.22_AM-removebg-preview.png";
+export const getBrandName = () => "Hajj Express BD ";
+export const getHomePageTitle = () => "Hajj Express BD ";
+export const getHomePageDescription = () => "Hajj Express BD ";
+export const getBrandLogo = () => "/Hajj-Express-BD-Logo.webp";
 export const getWhatsApp = () => "8801743214111";
 export const getCalendlyLink = () => "https://calendly.com/tessagency";
-export const getSlogan  = () => "Powered by TESS";
+export const getSlogan  = () => "Hajj Express BD ";
